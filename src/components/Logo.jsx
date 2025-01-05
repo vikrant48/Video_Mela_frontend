7132bd519c02fae18817fcf51c8d686782105867
vikrant48 <vikrantchauhan9794@gmail.com>
2025-01-05 20:30:47 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoIosVideocam } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+function Logo({ size = "30" }) {
+    return (
+        <>
+            <Link to={'/'} className="flex gap-2 items-center">
+                <IoIosVideocam
+                    size={size}
+                    color="#A855F7"
+                />
+                <span className="font-bold text-white">VideoMela</span>
+            </Link>
+        </>
+    );
+}
+
+export default Logo;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllVideos, makeVideosNull } from "../store/Slices/videoSlice";
+import { VideoList, Container } from "../components";
+import HomeSkeleton from "../skelton/homeSkelton.jsx";
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+function HomePage() {
+    const dispatch = useDispatch();
+    const videos = useSelector((state) => state.video?.videos?.docs);
+    const loading = useSelector((state) => state.video?.loading);
+    const hasNextPage = useSelector(
+        (state) => state.video?.videos?.hasNextPage
+    );
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        dispatch(getAllVideos({ page: 1, limit: 10 }));
+
+        return () => dispatch(makeVideosNull());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (loading) {
+            setIsLoading(true);
+        } else {
+            setIsLoading(false);
+        }
+    }, [loading]);
+
+    const fetchMoreVideos = useCallback(() => {
+        if (hasNextPage) {
+            dispatch(getAllVideos({ page: page + 1, limit: 10 }))
+                .then(() => {
+                    setPage((prev) => prev + 1);
+                })
+                .catch((error) => {
+                    console.error("Error loading more videos:", error);
+                    setIsLoading(false);
+                });
+        }
+    }, [page, hasNextPage, dispatch]);
+
+    return (
+        <Container>
+            <InfiniteScroll
+                dataLength={videos?.length || 0}
+                next={fetchMoreVideos}
+                hasMore={hasNextPage}
+                loader={isLoading && <HomeSkeleton />}
+                scrollableTarget="scrollable-container"
+            >
+                <div
+                    className="text-white mb-20 sm:m-0 max-h-screen w-full grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 overflow-y-scroll"
+                    id="scrollable-container"
+                >
+                    {videos?.map((video) => (
+                        <VideoList
+                            key={video._id}
+                            avatar={video.ownerDetails?.avatar.url}
+                            duration={video.duration}
+                            title={video.title}
+                            thumbnail={video.thumbnail?.url}
+                            createdAt={video.createdAt}
+                            views={video.views}
+                            channelName={video.ownerDetails.username}
+                            videoId={video._id}
+                        />
+                    ))}
+                </div>
+            </InfiniteScroll>
+        </Container>
+    );
+}
+
+export default HomePage;
