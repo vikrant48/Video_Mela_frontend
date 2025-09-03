e85cacf02aa744404769d0e7f07bba64e5037cfc
vikrant48 <vikrantchauhan9794@gmail.com>
2025-09-03 21:16:29 +0530

    update port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5173,
     proxy: {
       '/api/v1': 'https://videomela-backend.onrender.com'
     }
