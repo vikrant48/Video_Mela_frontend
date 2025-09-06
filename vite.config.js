7b8498658bab06b74e0f1006146dd03631996260
vikrant48 <vikrantchauhan9794@gmail.com>
2025-09-06 15:40:50 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      // '/api/v1': 'https://videomela-backend.onrender.com',
-      '/api/v1': 'http://localhost:8080',
+      '/api/v1': 'https://videomela-backend.onrender.com',
+      // '/api/v1': 'http://localhost:8080',
     }
   },
   plugins: [react()],
